491377213145f718b129e689ffff0add770fd7b8
Adam Banky <abanky92@gmail.com>
2017-08-06 11:30:21 -0400

    Updated Gulpfile 8/6/17

--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,96 @@
 // Load all gulp plugins
 var gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')({
-        pattern: '*'
-    });
+	plugins = require('gulp-load-plugins')({
+		pattern: '*'
+	});
 
 //Sass Compilation
-    gulp.task('scss', function() {
-        gulp.src('./scss/main.scss')
-            .pipe(plugins.plumber())
-            .pipe(plugins.sass({errLogToConsole: true}))
-            .pipe(gulp.dest('./css'))
-            .pipe(plugins.browserSync.reload({stream: true}));
-    });
+gulp.task('scss', function() {
+	gulp.src('./scss/main.scss')
+		.pipe(plugins.plumber())
+		.pipe(plugins.sass({
+			errLogToConsole: true
+		}))
+		.pipe(gulp.dest('./css'))
+		.pipe(plugins.browserSync.reload({
+			stream: true
+		}));
+});
 
 //Css Minification and concat
-    var cssFiles = './css/*.css',
-        cssDest = './dist/css';
+var cssFiles = './css/*.css',
+	cssDest = './dist/css';
 
-    gulp.task('minify-css', function() {
-        return gulp.src(cssFiles)
-            .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.autoprefixer({
-               browsers: ['last 2 versions'],
-               cascade: false
-           }))
-            .pipe(plugins.concat('styles.css'))
-            .pipe(gulp.dest(cssDest))
-            .pipe(plugins.cleanCss())
-            .pipe(plugins.rename({suffix: '.min'}))
-            .pipe(plugins.sourcemaps.write())
-            .pipe(gulp.dest(cssDest))
-            .pipe(plugins.browserSync.reload({stream: true}));
-    });
+gulp.task('minify-css', function() {
+	return gulp.src(cssFiles)
+		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(plugins.concat('styles.css'))
+		.pipe(gulp.dest(cssDest))
+		.pipe(plugins.cleanCss())
+		.pipe(plugins.rename({
+			suffix: '.min'
+		}))
+		.pipe(plugins.sourcemaps.write())
+		.pipe(gulp.dest(cssDest))
+		.pipe(plugins.browserSync.reload({
+			stream: true
+		}));
+});
 
 // JS Concat and uglify
-    var jsFiles = './js/*.js',
-        jsDest = './dist/scripts';
+var jsFiles = './js/*.js',
+	jsDest = './dist/scripts';
 
-        gulp.task('scripts', function() {
-            return gulp.src(jsFiles)
-            .pipe(plugins.sourcemaps.init())
-            .pipe(plugins.concat('scripts.js'))
-            .pipe(gulp.dest(jsDest))
-            .pipe(plugins.rename('scripts.min.js'))
-            .pipe(plugins.uglify())
-            .pipe(plugins.sourcemaps.write())
-            .pipe(gulp.dest(jsDest))
-            .pipe(plugins.browserSync.reload({stream: true}));
-        });
+gulp.task('scripts', function() {
+	return gulp.src(jsFiles)
+		.pipe(plugins.plumber())
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.concat('scripts.js'))
+		.pipe(gulp.dest(jsDest))
+		.pipe(plugins.rename('scripts.min.js'))
+		.pipe(plugins.uglify())
+		.pipe(plugins.sourcemaps.write())
+		.pipe(gulp.dest(jsDest))
+		.pipe(plugins.browserSync.reload({
+			stream: true
+		}));
+});
 
 // Image Minification
-    gulp.task('imagemin', function() {
-        gulp.src('./img/*')
-            .pipe(plugins.imagemin())
-            .pipe(gulp.dest('./dist/img'))
-            .pipe(plugins.browserSync.reload({stream: true}));
-    });
+var imgFiles = './img/**/*',
+	imgDest = './dist/scripts';
+
+gulp.task('imagemin', function() {
+	gulp.src(imgFiles)
+		.pipe(plugins.plumber())
+		.pipe(plugins.imagemin())
+		.pipe(gulp.dest(imgDest))
+		.pipe(plugins.browserSync.reload({
+			stream: true
+		}));
+});
 
 // Browser sync
-    gulp.task('serve', function() {
-        plugins.browserSync.init({
-            server: {
-                baseDir: './'
-            },
-            online: true
-        });
+gulp.task('serve', function() {
+	plugins.browserSync.init({
+		server: {
+			baseDir: './'
+		},
+		online: true
+	});
 
-        gulp.watch('./scss/**/*.scss', ['scss']);
-        gulp.watch('./css/*.css', ['minify-css']);
-        gulp.watch('./js/*.js', ['scripts']);
-        gulp.watch('./dist/js/*.js').on('change', plugins.browserSync.reload);
-        gulp.watch('./dist/css/*.css').on('change', plugins.browserSync.reload);
-        gulp.watch('./**/*.html').on('change', plugins.browserSync.reload);
-    });
+	gulp.watch('./scss/**/*.scss', ['scss']);
+	gulp.watch('./css/*.css', ['minify-css']);
+	gulp.watch('./js/*.js', ['scripts']);
+	gulp.watch('./dist/js/*.js').on('change', plugins.browserSync.reload);
+	gulp.watch('./dist/css/*.css').on('change', plugins.browserSync.reload);
+	gulp.watch('./**/*.html').on('change', plugins.browserSync.reload);
+});
 
 // RUN
 gulp.task('default', ['scss', 'minify-css', 'scripts', 'imagemin', 'serve']);
